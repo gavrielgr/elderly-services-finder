489fc3da1123ce559d65ef4613814a7bc1bf6585
gavrielgr <gavrielgr@walla.com>
2025-03-17 15:21:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Elderly Services Finder
 
 // This version number should match APP_VERSION in app.js
-const CACHE_VERSION = '1.997.20';
+const CACHE_VERSION = '1.997.21';
 const CACHE_NAME = `elderly-services-cache-v${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   '/',
