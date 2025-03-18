cddec0cc62192907aa9ef741ce2f4300ce3c6e46
gavriel.greenfeld@moovit.com <gavriel.greenfeld@moovit.com>
2025-03-18 21:50:28 +0200

    v1.99.65

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Elderly Services Finder
 
 // This version is injected during build
-const CACHE_VERSION = '1.99.65'; // Updated version
+const CACHE_VERSION = '1.99.66'; // Updated version
 const CACHE_NAME = `elderly-services-cache-v${CACHE_VERSION}`;
 const ASSETS_TO_CACHE = [
   './',
