121335d7a5c1926990f3e2e5ff8b5ba0430eab52
gavriel.greenfeld@moovit.com <gavriel.greenfeld@moovit.com>
2025-03-24 08:22:12 +0200

    v1.99.75

--- js/config/constants.js
+++ js/config/constants.js
@@ -1,6 +1,6 @@
 // These values will be replaced during build but work for local dev
-export const BUILD_TIMESTAMP = '2025-03-24T06:19:52.621Z';
-export const APP_VERSION = '1.99.75'; // Updated version
+export const BUILD_TIMESTAMP = '2025-03-24T06:22:12.562Z';
+export const APP_VERSION = '1.99.76'; // Updated version
 export const CACHE_VERSION = `${APP_VERSION}-${BUILD_TIMESTAMP}`;
 
 // Database constants
