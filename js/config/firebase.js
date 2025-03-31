417063279d081b7992a4d9fe243e1822d13af19e
gavriel.greenfeld@moovit.com <gavriel.greenfeld@moovit.com>
2025-03-31 21:51:57 +0300

    fix: export app from firebase config and update build script

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -12,6 +12,6 @@ export const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app); 
