b7031a3ab92e49fc25cb3044e626dc86f59779b5
gavriel.greenfeld@moovit.com <gavriel.greenfeld@moovit.com>
2025-03-31 18:28:30 +0300

    fix: update service worker registration and file paths

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ import { InstallManager } from './pwa/installManager.js';
 // Register Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/assets/sw-lJnnwrRA.js')
             .then(registration => {
                 console.log('ServiceWorker registration successful');
             })
