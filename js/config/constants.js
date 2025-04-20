8c659922091f537bca46877bcf7ec39f4ee1d1c8
gavriel.greenfeld@moovit.com <gavriel.greenfeld@moovit.com>
2025-04-20 23:23:47 +0300

    v1.99.116

--- js/config/constants.js
+++ js/config/constants.js
@@ -1,6 +1,6 @@
 // These values will be replaced during build but work for local dev
-export const BUILD_TIMESTAMP = '2025-04-20T20:14:51.280Z';
-export const APP_VERSION = '1.99.116'; // Updated version to match package.json
+export const BUILD_TIMESTAMP = '2025-04-20T20:23:45.329Z';
+export const APP_VERSION = '1.99.117'; // Updated version to match package.json
 export const CACHE_VERSION = `${APP_VERSION}-${BUILD_TIMESTAMP}`;
 
 // Database constants
